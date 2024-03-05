fa199b3ff072cb8cc85f6c4bba592ded73e78bb5
shinnosuke14202 <longdang14202@gmail.com>
2024-03-05 17:38:05 +0700

    test

--- app/src/main/java/ecom/mobile/app/AppApplication.java
+++ app/src/main/java/ecom/mobile/app/AppApplication.java
@@ -2,8 +2,10 @@ package ecom.mobile.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AppApplication {
 
 	public static void main(String[] args) {
---
+++ app/src/main/java/ecom/mobile/app/controllderdemo/Demo.java
@@ -0,0 +1,16 @@
+package ecom.mobile.app.controllderdemo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class Demo {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World";
+    }
+
+}
