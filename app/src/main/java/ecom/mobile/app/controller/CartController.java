d84f05966b03c6a2a54dc4eaf92b46f8bc01bf61
kienyk02 <levankienyk02@gmail.com>
2024-03-22 19:36:11 +0700

    cáºart controller

--- app/src/main/java/ecom/mobile/app/controller/CartController.java
+++ app/src/main/java/ecom/mobile/app/controller/CartController.java
@@ -1,13 +1,42 @@
 package ecom.mobile.app.controller;
 
+import ecom.mobile.app.model.Cart;
 import ecom.mobile.app.service.serviceInterface.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api")
 public class CartController {
     @Autowired
     CartService cartService;
+
+    @PostMapping("/add-cart")
+    public Cart addCart(@RequestBody Cart cart){
+        return cartService.addCart(cart);
+    }
+
+    @GetMapping("/carts/{user_id}")
+    public List<Cart> getCartsByUserId(@PathVariable("user_id") int user_id){
+        return cartService.getCartsByUserId(user_id);
+    }
+
+    @PostMapping("/cart/update/{cart_id}")
+    public void updateCartQuantity(@PathVariable("cart_id") int cart_id,@RequestParam("quantity") int quantity){
+        cartService.updateCartQuantity(cart_id,quantity);
+    }
+
+    @DeleteMapping("/cart/delete/{cart_id}")
+    public void deleteCartById(@PathVariable("cart_id") int cart_id){
+        cartService.deleteCartById(cart_id);
+    }
+
+    @DeleteMapping("/cart/delete-all/{user_id}")
+    public void deleteCartByUid(@PathVariable("user_id") int user_id){
+        cartService.deleteCartByUserId(user_id);
+    }
+
 }
